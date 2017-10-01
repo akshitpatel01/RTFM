130e686a140822fb64289764d25cb5e8148bd63e
mishal23 <shahmishal1998@gmail.com>
2017-10-01 21:03:17 +0530

    all done

---
+++ rtfm4.c
@@ -0,0 +1,22 @@
+/*
+	program to find the sum of the squaresof the integers of the given digit
+*/
+#include<stdio.h>
+
+#include<math.h>
+int main()
+{
+ int n,t=0,r,q;
+ printf("Enter the number to be tested: ");
+ scanf("%d",&n);
+ q=n;
+ do
+ {
+      r=q%10;
+      t+=r*r;
+      q=q/10;
+ }
+ while(q!=0);
+ printf("%d\n",t);
+
+}
