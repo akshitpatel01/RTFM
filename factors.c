d56849d62cd88464ddf5366081145ac7e1052526
mishal23 <shahmishal1998@gmail.com>
2017-10-01 20:18:40 +0530

    requested changes done

--- factors.c
+++ factors.c
@@ -6,16 +6,17 @@
 #include "string.h"
 int main()
 {
-	int i,number;
-	while(1)
+	int i,number,testcases=5;
+	while(testcases--)
 	{
+		int count=0;
 		printf("Enter a number: ");
 		scanf("%d",&number);
 		for(i=1;i<=number;i++)
 		{
 			if(number%i==0)
-				printf("%d ",i);
+				count++;
 		}
-		printf("\n");
+		printf("%d\n",count);
 	}
 }
